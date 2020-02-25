47beb85c5f5f27d8c09784b572d1aaf222255c09
Sanjit Verma <sanjitvermasw@gmail.com>
2020-02-25 13:27:13 -0500

    Level 2 Branch

---
+++ src/com/company/RosterGenerator.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class RosterGenerator {
+    public static void main(String[] args) {
+            final int max = 5;
+            int[] jerseyNums = new int[max];
+            int[] playerRating = new int[max];
+            Scanner file = new Scanner(System.in);

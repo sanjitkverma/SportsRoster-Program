9e39fb3a1ecae04e66dc9ead3a8f0e37c85bed6f
Sanjit Verma <sanjitvermasw@gmail.com>
2020-02-27 10:58:02 -0500

    LVL 5 Major Project Update Player Rankings

--- src/com/company/RosterGenerator.java
+++ src/com/company/RosterGenerator.java
@@ -10,62 +10,83 @@ public class RosterGenerator {
 
         try {
             for (int i = 0; i < numbers; i++) {
-                System.out.print("Enter player " + (i+1) + "'s jersey number (0-99): ");
+                System.out.print("Enter player " + (i + 1) + "'s jersey number (0-99): ");
                 int jersey = input.nextInt();
                 jerseys[i] = jersey;
 
-                System.out.print("Enter player " + (i+1) + "'s ranking(1-9): ");
+                System.out.print("Enter player " + (i + 1) + "'s ranking(1-9): ");
                 int ranking = input.nextInt();
                 rankings[i] = ranking;
                 System.out.println();
             }
-                if (jerseys[1] == jerseys[2]) {
-                    System.out.println("Error Jersey#1 same number as Jersey#2 "); }
-                if (jerseys[1] == jerseys[3]) {
-                    System.out.println("Error Jersey#1 same number as Jersey#3"); }
-                if (jerseys[1] == jerseys[4]) {
-                    System.out.println("Error Jersey#1 same number as Jersey#4"); }
-                if (jerseys[1] == jerseys[0]) {
-                    System.out.println("Error Jersey#1 same number as Jersey#5"); }
+            if (jerseys[1] == jerseys[2]) {
+                System.out.println("Error Jersey#1 same number as Jersey#2 ");
+            }
+            if (jerseys[1] == jerseys[3]) {
+                System.out.println("Error Jersey#1 same number as Jersey#3");
+            }
+            if (jerseys[1] == jerseys[4]) {
+                System.out.println("Error Jersey#1 same number as Jersey#4");
+            }
+            if (jerseys[1] == jerseys[0]) {
+                System.out.println("Error Jersey#1 same number as Jersey#5");
+            }
 
-                if (jerseys[2] == jerseys[1]) {
-                    System.out.println("Error Jersey#2 same number as Jersey#1 "); }
-                if (jerseys[2] == jerseys[3]) {
-                    System.out.println("Error Jersey#2 same number as Jersey#3"); }
-                if (jerseys[2] == jerseys[4]) {
-                    System.out.println("Error Jersey#2 same number as Jersey#4"); }
-                if (jerseys[2] == jerseys[0]) {
-                    System.out.println("Error Jersey#2 same number as Jersey#5"); }
+            if (jerseys[2] == jerseys[1]) {
+                System.out.println("Error Jersey#2 same number as Jersey#1 ");
+            }
+            if (jerseys[2] == jerseys[3]) {
+                System.out.println("Error Jersey#2 same number as Jersey#3");
+            }
+            if (jerseys[2] == jerseys[4]) {
+                System.out.println("Error Jersey#2 same number as Jersey#4");
+            }
+            if (jerseys[2] == jerseys[0]) {
+                System.out.println("Error Jersey#2 same number as Jersey#5");
+            }
 
-                if (jerseys[3] == jerseys[1]) {
-                    System.out.println("Error Jersey#3 same number as Jersey#1 "); }
-                if (jerseys[3] == jerseys[2]) {
-                    System.out.println("Error Jersey#3 same number as Jersey#2"); }
-                if (jerseys[3] == jerseys[4]) {
-                    System.out.println("Error Jersey#3 same number as Jersey#4"); }
-                if (jerseys[3] == jerseys[0]) {
-                    System.out.println("Error Jersey#3 same number as Jersey#5"); }
+            if (jerseys[3] == jerseys[1]) {
+                System.out.println("Error Jersey#3 same number as Jersey#1 ");
+            }
+            if (jerseys[3] == jerseys[2]) {
+                System.out.println("Error Jersey#3 same number as Jersey#2");
+            }
+            if (jerseys[3] == jerseys[4]) {
+                System.out.println("Error Jersey#3 same number as Jersey#4");
+            }
+            if (jerseys[3] == jerseys[0]) {
+                System.out.println("Error Jersey#3 same number as Jersey#5");
+            }
 
-                if (jerseys[4] == jerseys[1]) {
-                    System.out.println("Error Jersey#4 same number as Jersey#1 "); }
-                if (jerseys[4] == jerseys[2]) {
-                    System.out.println("Error Jersey#4 same number as Jersey#2"); }
-                if (jerseys[4] == jerseys[3]) {
-                    System.out.println("Error Jersey#4 same number as Jersey#3"); }
-                if (jerseys[4] == jerseys[0]) {
-                    System.out.println("Error Jersey#4 same number as Jersey#5"); }
+            if (jerseys[4] == jerseys[1]) {
+                System.out.println("Error Jersey#4 same number as Jersey#1 ");
+            }
+            if (jerseys[4] == jerseys[2]) {
+                System.out.println("Error Jersey#4 same number as Jersey#2");
+            }
+            if (jerseys[4] == jerseys[3]) {
+                System.out.println("Error Jersey#4 same number as Jersey#3");
+            }
+            if (jerseys[4] == jerseys[0]) {
+                System.out.println("Error Jersey#4 same number as Jersey#5");
+            }
 
-                if (jerseys[0] == jerseys[1]) {
-                    System.out.println("Error Jersey#5 same number as Jersey#1 "); }
-                if (jerseys[0] == jerseys[2]) {
-                    System.out.println("Error Jersey#5 same number as Jersey#2"); }
-                if (jerseys[0] == jerseys[3]) {
-                    System.out.println("Error Jersey#5 same number as Jersey#3"); }
-                if (jerseys[0] == jerseys[4]) {
-                    System.out.println("Error Jersey#5 same number as Jersey#4"); }
+            if (jerseys[0] == jerseys[1]) {
+                System.out.println("Error Jersey#5 same number as Jersey#1 ");
+            }
+            if (jerseys[0] == jerseys[2]) {
+                System.out.println("Error Jersey#5 same number as Jersey#2");
+            }
+            if (jerseys[0] == jerseys[3]) {
+                System.out.println("Error Jersey#5 same number as Jersey#3");
+            }
+            if (jerseys[0] == jerseys[4]) {
+                System.out.println("Error Jersey#5 same number as Jersey#4");
+            }
 
             String menu = input.nextLine();
             while (!close) {
+                System.out.println(" ");
                 System.out.println("MENU");
                 System.out.println("u - Update player ranking");
                 System.out.println("a - Output players above a ranking");
@@ -82,11 +103,25 @@ public class RosterGenerator {
                     System.out.println("CURRENT ROSTER");
                     int i = 0;
                     while (i < numbers) {
-                        System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseys[i] + ", Rating: " + jerseys[i]);
+                        System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseys[i] + ", Rating: " + rankings[i]);
                         System.out.println("...");
                         i++;
+                        }
                     }
-                }
+                    else if (menu.contains("u")) {
+                        System.out.print("Enter a jersey number: ");
+                        int jersey = input.nextInt();
+                        int i;
+                    for (i = 0; i < numbers; i++) {
+                            if (jerseys[i] == jersey) {
+                                break;
+                            }
+                        }
+                        System.out.print("Enter new rating: ");
+                        int rating = input.nextInt();
+                        rankings[i] = rating;
+                        menu = input.nextLine();
+                    }
             }
         } catch (Exception e) {
             System.out.println("Invalid Parameter try again");
